10f4e1f81470f987f1f41d7dc9333f819fe78cd9
Etienne Koekemoer <e@ek.pe>
2018-09-06 09:30:49 +0200

    Added leader board service

---
+++ sonarQuest-backend/src/main/java/com/viadee/sonarQuest/controllers/LeaderController.java
@@ -0,0 +1,22 @@
+package com.viadee.sonarQuest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viadee.sonarQuest.entities.User;
+import com.viadee.sonarQuest.repositories.UserRepository;
+
+@RequestMapping("/leaders")
+@RestController
+public class LeaderController {
+	private @Autowired UserRepository userRepository;
+
+	@GetMapping()
+	public List<User> getLeaders() {
+		return userRepository.getLeaders();
+	}
+}
--- sonarQuest-backend/src/main/java/com/viadee/sonarQuest/repositories/UserRepository.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarQuest/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package com.viadee.sonarQuest.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.viadee.sonarQuest.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
-
+    
+    @Query("FROM User ORDER BY level.level DESC, xp DESC")
+    List<User> getLeaders();
 }
